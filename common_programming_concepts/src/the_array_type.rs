ccabe1fd24bc8efb0817ef685b355b3874c570d6
little-chloe <laptrinhcungtony@gmail.com>
2024-11-10 23:48:49 +0700

    finish section 3

---
+++ common_programming_concepts/src/the_array_type.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+}
+
+#![allow(unused)]
+fn main() {
+let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+}
+
+#![allow(unused)]
+fn main() {
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+}
+
+#![allow(unused)]
+fn main() {
+    let a = [3; 5];
+}
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+}
