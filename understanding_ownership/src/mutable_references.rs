4f3c2ad43bd54ac344c7aeb8ec61cbd800e31bc1
little-chloe <laptrinhcungtony@gmail.com>
2024-11-12 23:55:37 +0700

    fisnish chapter 4

---
+++ understanding_ownership/src/mutable_references.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+// ============================================
+fn main() {
+    let mut s = String::from("hello");
+
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+}
+
+// ============================================
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{r3}");
+}
